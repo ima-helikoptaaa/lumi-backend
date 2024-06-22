565abb046a068cae52c6b636dfddf70c6ddc6a89
Aditya Jha <4adityajha@gmail.com>
2024-06-22 11:25:30 +0530

    add initialization context manager app_init

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from app.core.config import settings
+from app.api.endpoints.api import api_router
 
-@app.get("/")
-def starter():
-    return {"message": "starter"}
+@asynccontextmanager
+async def app_init(app: FastAPI):
+    app.include_router(api_router, prefix=settings.API_V1_STR)
+    yield
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    lifespan=app_init,
+)
